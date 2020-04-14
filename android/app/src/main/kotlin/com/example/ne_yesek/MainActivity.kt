1ceee1b180b08ba1dff49fefdfc00db061169bc2
Çağrı Aydın <cagriaydin@Cagrs-MacBook-Pro.local>
2020-04-15 00:06:39 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/ne_yesek/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ne_yesek
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+
+String userModelToJson(UserModel data) => json.encode(data.toJson());
+
+class UserModel {
+  String id;
+  String title;
+  String color;
+  String image;
+  List<Food> foods;
+
+  UserModel({
+    this.id,
+    this.title,
+    this.color,
+    this.image,
+    this.foods,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: json["id"],
+        title: json["title"],
+        color: json["color"],
+        image: json["image"],
+        foods: List<Food>.from(json["foods"].map((x) => Food.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "color": color,
+        "image": image,
+        "foods": List<dynamic>.from(foods.map((x) => x.toJson())),
+      };
+}
+
+class Food {
+  String id;
+  String title;
+  String detail;
+  String color;
+  bool favorite;
+  String date;
+  String image;
+
+  Food({
+    this.id,
+    this.title,
+    this.detail,
+    this.color,
+    this.favorite,
+    this.date,
+    this.image,
+  });
+
+  factory Food.fromJson(Map<String, dynamic> json) => Food(
+        id: json["id"],
+        title: json["title"],
+        detail: json["detail"],
+        color: json["color"],
+        favorite: json["favorite"],
+        date: json["date"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "detail": detail,
+        "color": color,
+        "favorite": favorite,
+        "date": date,
+        "image": image,
+      };
+}
